01750802b2be0801a04009fc8bdf2b4510ed5e62
kevinjfa <kevin.fa7@outlook.com>
2025-01-10 18:45:14 +0000

    RandomUserAdapter class finished

--- src/main/java/es/software/ulpgc/kata5/RandomUserAdapter.java
+++ src/main/java/es/software/ulpgc/kata5/RandomUserAdapter.java
@@ -1,4 +1,15 @@
 package es.software.ulpgc.kata5;
 
+import org.json.JSONObject;
+
+import java.sql.Date;
+
 public class RandomUserAdapter implements UserAdapter {
+    public User adapt(JSONObject user) {
+        String name = user.getJSONObject("name").getString("first");
+        String surname = user.getJSONObject("name").getString("last");
+        Date date = Date.valueOf(user.getJSONObject("dob").getString("date").split("T")[0]);
+        String email = user.getString("email");
+        return new User(name, surname, date, email);
+    }
 }
