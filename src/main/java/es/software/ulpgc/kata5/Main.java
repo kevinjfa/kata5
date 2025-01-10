3eb4ade421fdbc2be5c2e7a14e26cca4b96cad98
kevinjfa <kevin.fa7@outlook.com>
2025-01-10 18:52:34 +0000

    Main finished, program working

--- src/main/java/es/software/ulpgc/kata5/Main.java
+++ src/main/java/es/software/ulpgc/kata5/Main.java
@@ -3,5 +3,9 @@ package es.software.ulpgc.kata5;
 public class Main {
     public static void main(String[] args) {
         UserAdapter adapter = new RandomUserAdapter();
+        UserLoader loader = new RandomUserLoader();
+        for (int i = 0; i < 50; i++) {
+            System.out.println(adapter.adapt(loader.load()));
+        }
     }
 }
